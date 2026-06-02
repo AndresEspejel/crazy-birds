00d3dc49d943e715bc5cd320587652499db1b1f9
itzcode <itzcode>
2026-06-02 00:31:16 -0600

    video 80

---
+++ scenes/player/player.gd
@@ -0,0 +1,23 @@
+extends RigidBody2D
+
+var is_dragged = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sleeping_state_changed.connect(_on_sleeping_state_changed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta: float) -> void:
+	if is_dragged:
+		position = get_global_mouse_position()
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("drag"):
+		is_dragged = true
+	if event.is_action_released("drag"):
+		is_dragged = false
+		freeze = false
+
+func  _on_sleeping_state_changed():
+	queue_free()
